6d5b838853bb99bce63fc7608e1df18695b2c31f
Sezer Turkdal <sezer.turkdal@hotmail.com>
2022-10-29 21:34:50 +0300

    first commit

---
+++ InstaCloneFirebase/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  InstaCloneFirebase
+//
+//  Created by Sezer on 26.10.2022.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
